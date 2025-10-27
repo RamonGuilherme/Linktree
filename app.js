854ddb37801983f5b7a3fb13bdbc0645ff52f7c3
Ramon Guilherme <64100250+RamonGuilherme@users.noreply.github.com>
2025-10-27 16:50:15 -0300

    Javascript
    Função de compartilhar

---
+++ app.js
@@ -0,0 +1,14 @@
+
+function share(){
+			if (navigator.share !== undefined) {
+				navigator.share({
+					title: 'O título da sua página',
+					text: 'Um texto de resumo',
+					url: 'https://seusite.com/sua_url',
+				})
+				.then(() => console.log('Successful share'))
+				.catch((error) => console.log('Error sharing', error));
+			}
+		}
+
+    
